aa7d60cd85cd4d8a4553884d2ad32b31f4402e06
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-02 10:05:41 -1000

    completed w stretch goals

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,8 +7,30 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
-
+  
   // do your work here
+
+  // method 1;
+  // let fibArr = [0,1];
+  // for (let i = 1; fibArr[i]<=maxFibValue; i++) {
+  //   fibArr.push(fibArr[i-1]+fibArr[i]);
+  //   if (fibArr[i] % 2 === 0 && fibArr[i] <= maxFibValue) {
+  //     sum += fibArr[i];
+  //   }
+  // }
+
+  // method 2;
+  let n1 = 0;
+  let n2 = 1;
+  let nextFibNum = 0;
+  while (nextFibNum <= maxFibValue) {
+    nextFibNum = n1 + n2;
+    n1 = n2;
+    n2 = nextFibNum;
+    if (nextFibNum % 2 === 0 && nextFibNum <= maxFibValue) {
+      sum += nextFibNum;
+    }
+  }
 
   return sum;
 }
@@ -19,8 +41,30 @@ function _highestFibonacciNumber (maxFibValue){
 
   //define your base case, validate your input
 
+  // method 1
+  // let fibArr = [0,1];
+  
+  // //do your work here
+  // if (maxFibValue > 1) {
+  //   for (let i = 1; fibArr[i]<=maxFibValue; i++) {
+  //     if (fibArr[i] <= maxFibValue) {
+  //       fibArr.push(fibArr[i-1]+fibArr[i]);
+  //     }
+  //   }
+  // }
+  // highest = fibArr[fibArr.length-2];
 
-  //do your work here
+  // method 2
+  let n1 = 0;
+  let n2 = 1;
+  let nextFibNum = 0;
+  while (nextFibNum <= maxFibValue) {
+    nextFibNum = n1 + n2;
+    n1 = n2;
+    n2 = nextFibNum;
+    }
+
+  highest = n1;
 
   return highest;
 };
